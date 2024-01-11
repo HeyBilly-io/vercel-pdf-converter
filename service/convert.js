e5c7a7e7ad5f1b683187a1356bafb9c378444b92
Jpec57 <jean.paul.bella@hotmail.fr>
2024-01-11 15:19:58 +0100

    working

--- service/convert.js
+++ service/convert.js
@@ -1,60 +1,73 @@
 import chromium from '@sparticuz/chromium-min'
 import puppeteer from 'puppeteer-core'
+import { executablePath } from 'puppeteer'
 
 async function getBrowser() {
-  // local development is broken for this 👇
-  // but it works in vercel so I'm not gonna touch it
+  // 	//npx playwright install
+  //   const browser = await chromium.launch({
+  //     args: ['--no-sandbox'], // Add any additional args you need
+  //   })
+  //   return browser
+
+  //   // local development is broken for this 👇
+  //   // but it works in vercel so I'm not gonna touch it
   return puppeteer.launch({
-    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+    args: [
+      ...chromium.args,
+      '--hide-scrollbars',
+      '--disable-web-security',
+      '--arch=arm64',
+    ],
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v119.0.2/chromium-v119.0.2-pack.tar`
-    ),
+    executablePath: executablePath(),
+
+    // executablePath: await chromium.executablePath(
+    //   `https://github.com/Sparticuz/chromium/releases/download/v119.0.2/chromium-v119.0.2-pack.tar`
+    // ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
-  });
+  })
 }
 
-
 export const getPdf = async (url) => {
+  // Start headless chrome instance
+  const browser = await getBrowser()
+  const page = await browser.newPage()
+  console.log('page generated')
+  // Visit URL and wait until everything is loaded (available events: load, domcontentloaded, networkidle0, networkidle2)
+  await page.goto(url, { waitUntil: 'networkidle2', timeout: 8000 })
+  //   await page.goto(url, { waitUntil: 'networkidle', timeout: 8000 })
 
-	// Start headless chrome instance
-	const browser = await getBrowser()
-	const page = await browser.newPage()
+  // Scroll to bottom of page to force loading of lazy loaded images
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0
+      const distance = 100
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight
+        window.scrollBy(0, distance)
+        totalHeight += distance
 
-	// Visit URL and wait until everything is loaded (available events: load, domcontentloaded, networkidle0, networkidle2)
-	await page.goto(url, { waitUntil: 'networkidle2', timeout: 8000 })
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer)
+          resolve()
+        }
+      }, 5)
+    })
+  })
 
-	// Scroll to bottom of page to force loading of lazy loaded images
-	await page.evaluate(async () => {
-		await new Promise((resolve) => {
-			let totalHeight = 0
-			const distance = 100
-			const timer = setInterval(() => {
-				const scrollHeight = document.body.scrollHeight
-				window.scrollBy(0, distance)
-				totalHeight += distance
+  // Tell Chrome to generate the PDF
+  await page.emulateMediaType('screen')
+  const buffer = await page.pdf({
+    format: 'A4',
+    displayHeaderFooter: true,
+    headerTemplate: '',
+    footerTemplate: '',
+    printBackground: true,
+  })
 
-				if (totalHeight >= scrollHeight) {
-					clearInterval(timer)
-					resolve()
-				}
-			}, 5)
-		})
-	})
+  // Close chrome instance
+  await browser.close()
 
-	// Tell Chrome to generate the PDF
-	await page.emulateMediaType('screen')
-	const buffer = await page.pdf({
-		format: 'A4',
-		displayHeaderFooter: true,
-		headerTemplate: '',
-		footerTemplate: '',
-		printBackground: true
-	})
-
-	// Close chrome instance
-	await browser.close()
-
-	return buffer
+  return buffer
 }
