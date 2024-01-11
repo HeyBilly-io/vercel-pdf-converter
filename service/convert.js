df31c16d8a597c6080ee781055d94946d4fd41ff
Jpec57 <jean.paul.bella@hotmail.fr>
2024-01-11 15:58:00 +0100

    Cheating

--- service/convert.js
+++ service/convert.js
@@ -1,6 +1,6 @@
 import chromium from '@sparticuz/chromium-min'
 import puppeteer from 'puppeteer-core'
-// import { executablePath } from 'puppeteer'
+import { executablePath } from 'puppeteer'
 
 async function getBrowser() {
   //   // local development is broken for this 👇
@@ -12,8 +12,8 @@ async function getBrowser() {
       '--disable-web-security',
       '--arch=arm64',
     ],
-	  defaultViewport: chromium.defaultViewport,
-	// For dev
+    defaultViewport: chromium.defaultViewport,
+    // For dev
     // executablePath: executablePath(),
     executablePath: await chromium.executablePath(
       `https://github.com/Sparticuz/chromium/releases/download/v119.0.2/chromium-v119.0.2-pack.tar`
@@ -21,6 +21,43 @@ async function getBrowser() {
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   })
+}
+
+export const getPdfStream = async (url) => {
+  // Start headless chrome instance
+  const browser = await getBrowser()
+  const page = await browser.newPage()
+  console.log('page generated')
+  // Visit URL and wait until everything is loaded (available events: load, domcontentloaded, networkidle0, networkidle2)
+  await page.goto(url, { waitUntil: 'networkidle2', timeout: 8000 })
+  //   await page.goto(url, { waitUntil: 'networkidle', timeout: 8000 })
+
+  // Scroll to bottom of page to force loading of lazy loaded images
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0
+      const distance = 100
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight
+        window.scrollBy(0, distance)
+        totalHeight += distance
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer)
+          resolve()
+        }
+      }, 5)
+    })
+  })
+
+  // Tell Chrome to generate the PDF
+  await page.emulateMediaType('screen')
+  const pdfStream = await page.createPDFStream()
+
+  // Close chrome instance
+  // await browser.close()
+
+  return pdfStream
 }
 
 export const getPdf = async (url) => {
